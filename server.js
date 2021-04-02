0dfefcd4d9d67fbf02f99a50eb1c4c69dd72f201
Shelby <shelbypkelly@gmail.com>
2021-04-02 00:37:34 -0400

    add index.html to express static. third attempt to fix heroku deploy

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/public"));
+  app.use(express.static("client/public/index.html"));
 }
 
 // Start the API server
